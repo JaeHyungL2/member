44267bc758b755253956fb033eea23cd5677e135
JaeHyungL2 <150316684+JaeHyungL2@users.noreply.github.com>
2024-03-04 18:52:15 +0900

    second update

--- src/main/java/com/codingrecipe/member/controller/HomeController.java
+++ src/main/java/com/codingrecipe/member/controller/HomeController.java
@@ -3,7 +3,7 @@ package com.codingrecipe.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+
 public class HomeController {
     //가장먼저!가장기본페이지요청시 메소드호출하게끔해놔야함.ㅋ.
     @GetMapping("/")
--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -1,11 +1,22 @@
 package com.codingrecipe.member.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //스프링 빈으로 등록시켜주기위해 (객체지..컨테이너가 관리하도록, 우린 주입만받기위해 ㅋ)
+@Controller
 public class MemberController {
-    @GetMapping("/member/save")
+    @GetMapping("/jh/save")
     public String saveForm() {
         return "save";
+    }
+
+    @PostMapping("/jh/save")
+    public String save(@RequestParam("Email") String Email) {
+        System.out.println("MemberController.save()");
+        System.out.println("memberEmail=" + Email);
+        return "index";
     }
 }
